9640eb1a895158ae578ae82db61a64881d861ffb
Natnael Sisay <nsisay49@gmail.com>
2025-10-29 08:29:13 +0000

    basic key maps

--- init.lua
+++ init.lua
@@ -9,16 +9,31 @@ vim.opt.expandtab = true   -- Convert tabs to spaces
 vim.opt.smartindent = true -- Auto-indent new lines
 vim.opt.autoindent = true  -- Copy indent from current line when starting a new one
 
--- keymaps
+-- leader key
+vim.g.mapleader = " " -- set leader to space (you can change if you use another)
 
---commands
+-- keymaps
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- <leader>kk to exit insert mode
+keymap("i", "kk", "<Esc>", opts)
+
+-- <leader>s to save
+keymap("n", "<leader>w", ":w<CR>", opts)
+
+-- <leader>q to quit
+keymap("n", "<leader>q", ":q<CR>", opts)
+
+-- optional: save + quit quickly
+keymap("n", "<leader>wq", ":wq<CR>", opts)
+
+-- user commands
 vim.api.nvim_create_user_command('NvimConfig', 'cd ~/.config/nvim', {})
 vim.api.nvim_create_user_command('ZelConfig', 'cd ~/.config/zellij', {})
 vim.api.nvim_create_user_command('AlaConfig', 'cd ~/.config/alacritty', {})
 vim.api.nvim_create_user_command('CodiumConfig', 'cd ~/.config/VSCodium', {})
 vim.api.nvim_create_user_command('CodeConfig', 'cd ~/.config/Code', {})
-
-
 vim.api.nvim_create_user_command('Projects', 'cd ~/Documents', {})
 
 require("config.lazy")
