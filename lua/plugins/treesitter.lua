a28a7773655e88057d97f1b42b1fd61d250f1c39
Nati <nsisay49@gmail.com>
2025-11-28 09:05:09 +0300

    indentation vertical lines

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,11 +7,11 @@ return {
   config = function()
     local ts_configs = require("nvim-treesitter.configs")
     ts_configs.setup({
-      ensure_installed = { "python", "java", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline","javascript", "typescript"},
-      sync_install = false,
-      auto_install = true,
-      highlight  = {enable = true},
-      indent = {enable = true},
+      ensure_installed = { "python", "java", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript" },
+      sync_install     = false,
+      auto_install     = true,
+      highlight        = { enable = true },
+      indent           = { enable = true },
     })
   end,
 }
