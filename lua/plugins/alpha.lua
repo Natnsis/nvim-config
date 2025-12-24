a0d62f8292a9f37dec7a604f14962b2b9c1ec228
Nati <nsisay49@gmail.com>
2025-12-24 18:23:03 +0300

    dash and theme

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,28 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"echasnovski/mini.icons",
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		-- require("alpha").setup(require("alpha.themes.dashboard").config)
-		require("alpha").setup(require("alpha.themes.theta").config)
-	end,
+  "goolord/alpha-nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local alpha = require("alpha")
+    local theta = require("alpha.themes.theta")
+
+    -- 🔥 Change ONLY the big text
+    theta.header.val = {
+      " ██████╗ ██████╗  ██████╗ █████╗ ",
+      "██╔═══██╗██╔══██╗██╔════╝██╔══██╗",
+      "██║   ██║██████╔╝██║     ███████║",
+      "██║   ██║██╔══██╗██║     ██╔══██║",
+      "╚██████╔╝██║  ██║╚██████╗██║  ██║",
+      " ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝",
+      "",
+      "          O R C A - D E V          ",
+    }
+
+    -- optional: change color
+    theta.header.opts.hl = "Type" -- try: "Constant", "Statement"
+
+    alpha.setup(theta.config)
+  end,
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,19 +1,33 @@
 return {
-	"navarasu/onedark.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("onedark").setup({
-			style = "darker", -- options: dark, darker, cool, deep, warm, warmer, light
-			transparent = false,
-			code_style = {
-				comments = "italic",
-				keywords = "italic",
-				functions = "none",
-				strings = "none",
-				variables = "none",
-			},
-		})
-		require("onedark").load()
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha", -- mocha, macchiato, frappe, latte
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      term_colors = true,
+      styles = {
+        comments = { "italic" },
+        keywords = { "italic" },
+        functions = {},
+        variables = {},
+        strings = {},
+      },
+      color_overrides = {},
+      highlight_overrides = {
+        mocha = {
+          -- optional: tweak specific colors
+          Normal = { bg = "#1e1e2e" }, -- like VSCode dark
+          Comment = { fg = "#6c7086", style = { "italic" } },
+        },
+      },
+    })
+    vim.cmd("colorscheme catppuccin")
+  end,
 }
