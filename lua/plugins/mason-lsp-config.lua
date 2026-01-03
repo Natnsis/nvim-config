0344a1e535495aa0c871be1a8f1099f4b05e998f
Nati <nsisay49@gmail.com>
2026-01-03 19:04:10 +0300

    dart lsp

--- lua/plugins/mason-lsp-config.lua
+++ lua/plugins/mason-lsp-config.lua
@@ -1,63 +1,71 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-	},
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "mason-org/mason.nvim" },
-		opts = {
-			ensure_installed = { "lua_ls", "ts_ls" },
-			automatic_installation = true,
-		},
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "ts_ls" },
+      automatic_installation = true,
+    },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- ✅ New API style (no setup())
-			vim.lsp.config.lua_ls = {
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						workspace = { checkThirdParty = false },
-					},
-				},
-			}
+      -- ✅ New API style (no setup())
+      vim.lsp.config.lua_ls = {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+          },
+        },
+      }
 
-			vim.lsp.config.ts_ls = {
-				capabilities = capabilities,
-			}
+      vim.lsp.config.ts_ls = {
+        capabilities = capabilities,
+      }
 
-			-- Enable the servers
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("ts_ls")
+      vim.lsp.config.dartls = {
+        capabilities = capabilities,
+        cmd = { "dart", "language-server", "--protocol=lsp" },
+        filetypes = { "dart" },
+        root_markers = { "pubspec.yaml" },
+      }
 
-			-- Diagnostics
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			})
+      -- Enable the servers
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("dartls")
 
-			-- Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP Goto Definition" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
-		end,
-	},
+      -- Diagnostics
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP Goto Definition" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+    end,
+  },
 }
